cdc46d40afdd68bbb56d1065578037ca24a33eeb
Douglas Juarez <dfjuarezreyes21@gmail.com>
2022-07-20 10:42:25 -0600

    interstitial ads load

--- src/Screens/AdsScreen.tsx
+++ src/Screens/AdsScreen.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { View, Text, Button } from 'react-native'
 import { InterstitialAd, AdEventType, BannerAd, TestIds, BannerAdSize } from 'react-native-google-mobile-ads';
 
-const adUnitId = __DEV__ ? TestIds.APP_OPEN : '';
+const adUnitId = __DEV__ ? TestIds.INTERSTITIAL : '';
 
 const interstitial = InterstitialAd.createForAdRequest(adUnitId, {
     requestNonPersonalizedAdsOnly: true,
@@ -14,42 +14,40 @@ export const AdsScreen = () => {
     const [loaded, setLoaded] = useState(true);
 
     useEffect(() => {
-        interstitial.addAdEventListener(AdEventType.CLOSED, () => {
-            setLoaded(true);
+
+        const unsubscribe = interstitial.addAdEventsListener(({ type }) => {
+            type === AdEventType.LOADED && setLoaded(true);
+            type === AdEventType.CLOSED && loadAd();
         })
 
-        interstitial.load();
-        //setDisabled(false);
+        loadAd();
+
+        return unsubscribe;
     }, []);
 
-    // useEffect(() => {
-    //   const unsubcribed = interstitial.addAdEventListener(AdEventType.LOADED, () => {
-    //     setLoaded(true)
-    //   });
-    //   interstitial.load();
+    const loadAd = () => {
+        setLoaded(false);
+        interstitial.load()
+    }
 
-    //   return unsubcribed;
-    // }, []);
+    const showAds = () => {
+        if (!loaded) {
+            console.log('no loaded ad [null]')
+            return null
+        }
+        interstitial.show();
+    }
 
-    // if (!loaded) {
-    //   return null;
-    // }
     return (
         <View style={{ backgroundColor: 'red', flex: 1 }}>
             <Text>AdsScreen</Text>
-            {
-                loaded && (
-                    <Button
-                        //disabled={disabled}
-                        title="Show Interstitial"
-                        onPress={() => {
 
-                            interstitial.show();
-                            setLoaded(false);
-                        }}
-                    />
-                )
-            }
+            <Button
+                title="Show Interstitial"
+                onPress={() => {
+                    showAds()
+                }}
+            />
 
             <View style={{ position: 'absolute', bottom: 0 }}>
                 <BannerAd unitId={TestIds.BANNER} size={BannerAdSize.FULL_BANNER} />
