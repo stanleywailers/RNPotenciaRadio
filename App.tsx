edecb21ec2108dbcfa0ed62f35bab1fff6eaa7e2
Douglas Juarez <dfjuarezreyes21@gmail.com>
2022-07-22 17:04:07 -0600

    icon notification added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import { View } from 'react-native'
 import { HomeScreen } from './src/Screens/HomeScreen';
-import { AdsScreen } from './src/Screens/AdsScreen';
+// import { AdsScreen } from './src/Screens/AdsScreen';
 
 const App = () => {
-
-
-
   return (
     <View style={{ flex: 1 }}>
       <HomeScreen />
--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -10,6 +10,7 @@ import { QueueInitalTracksService } from '../Services/QueueInitalTracksService';
 import TrackPlayer from 'react-native-track-player';
 import { useOnTogglePlayback } from '../Hooks/useOnTogglePlayback';
 import { AdEventType, BannerAd, BannerAdSize, InterstitialAd, TestIds } from 'react-native-google-mobile-ads';
+import messaging from '@react-native-firebase/messaging';
 
 const adUnitIdBanner = __DEV__
   ? TestIds.BANNER
@@ -51,6 +52,17 @@ export const HomeScreen = () => {
     run();
   }, []);
 
+  // Muestra el FCM token en consola. 
+  useEffect(() => {
+    const checkToken = async () => {
+      const fcmToken = await messaging().getToken();
+      if (fcmToken) {
+        console.log(fcmToken);
+      }
+    }
+
+    checkToken();
+  }, [])
 
   useEffect(() => {
     const appStateListener = AppState.addEventListener(
