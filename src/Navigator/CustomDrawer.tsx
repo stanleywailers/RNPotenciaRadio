43d51e70fb78ae810e73a59c4126f535f1a3b8ce
ricardo.melara <ricardo.melara@novatechdev.com>
2022-07-28 16:33:04 -0600

    icon was added

--- src/Navigator/CustomDrawer.tsx
+++ src/Navigator/CustomDrawer.tsx
@@ -2,23 +2,31 @@ import React from 'react'
 import { createDrawerNavigator, DrawerContentComponentProps, DrawerContentScrollView, DrawerNavigationOptions } from '@react-navigation/drawer';
 import { HomeScreen } from '../Screens/HomeScreen';
 import { AdsScreen } from '../Screens/AdsScreen';
-import { Image, Text, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { DrawerMenuCell } from '../Components/DrawerMenuCell';
 import { NewsScreen } from '../Screens/NewsScreen';
 import { SettingsScreen } from '../Screens/SettingsScreen';
+import { DrawerActions, useNavigation } from '@react-navigation/native';
 
 
 const Drawer = createDrawerNavigator();
 
-export const CustomDrawer = () => {
+export const CustomDrawer = ({}) => {
   return (
     <Drawer.Navigator  
+    screenOptions= {({navigation}) => ({ headerLeft: () =>
+      <TouchableOpacity  onPress={() =>  navigation.dispatch(DrawerActions.openDrawer()) }>
+      <Icon name="reorder-three-sharp" size={40} color="black" style={{marginLeft:10}} />
+    </TouchableOpacity>
+    })}
+    
+ 
     drawerContent={ (props) => <SideMenu  {...props}/>}
     initialRouteName="HomeScreen">
-    <Drawer.Screen name="HomeScreen" component={HomeScreen} />
-    <Drawer.Screen name="NewsScreen" component={NewsScreen} />
-    <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+    <Drawer.Screen  options={ () => ({ title:'Inicio'})} name="HomeScreen" component={HomeScreen}  />
+    <Drawer.Screen options={ () => ({ title:'Noticias'})} name="NewsScreen" component={NewsScreen} />
+    <Drawer.Screen options={ () => ({ title:'Configuraciones'})} name="SettingsScreen" component={SettingsScreen} />
     <Drawer.Screen name="AdsScreen" component={AdsScreen} />
   </Drawer.Navigator>
   )
