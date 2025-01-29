ffbdf4949031294ecc6a7c7af8a55176b555bff0
douglas <dfjuarezreyes21@gmail.com>
2025-01-29 09:52:15 -0600

    rn-track-player new implementation config

--- index.js
+++ index.js
@@ -4,16 +4,16 @@
 import messaging from '@react-native-firebase/messaging';
 
 // handle for message when the app is background
-messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-    console.log('Message handled in the background!', remoteMessage);
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
 });
 
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import TrackPlayer from 'react-native-track-player';
 import App from './App';
-import { name as appName } from './app.json';
-import { PlaybackService } from './src/Services/PlaybackService';
+import {name as appName} from './app.json';
+// import {PlaybackService} from './src/Services/PlaybackService';
 
 AppRegistry.registerComponent(appName, () => App);
-TrackPlayer.registerPlaybackService(() => PlaybackService);
-
+TrackPlayer.registerPlaybackService(() => require('./service'));
+// TrackPlayer.registerPlaybackService(() => PlaybackService);
--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useEffect, useState} from 'react';
-import {Image, StyleSheet, View} from 'react-native';
+import {Button, Image, StyleSheet, Text, View} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import LinearGradient from 'react-native-linear-gradient';
 import analytics from '@react-native-firebase/analytics';
@@ -8,46 +8,107 @@ import analytics from '@react-native-firebase/analytics';
 import {PlayPauseButton} from '../Components/PlayPauseButton';
 import {SetupServicePlayer} from '../Services/SetupServicePlayer';
 import {QueueInitalTracksService} from '../Services/QueueInitalTracksService';
-import TrackPlayer from 'react-native-track-player';
+import TrackPlayer, {
+  AppKilledPlaybackBehavior,
+  Capability,
+  State,
+  usePlaybackState,
+} from 'react-native-track-player';
 
 import {BannerAd, BannerAdSize, TestIds} from 'react-native-google-mobile-ads';
 import messaging from '@react-native-firebase/messaging';
+
+import localArtwork from '../Assets/logo_cuadrado.png';
 
 const adUnitIdBanner = __DEV__
   ? TestIds.BANNER
   : 'ca-app-pub-8582719280960685/3307949149';
 
 export const HomeScreen = () => {
-  const [isPlayerReady, setIsPlayerReady] = useState<boolean>(false);
+  const playbackState = usePlaybackState();
+  // const [isPlayerReady, setIsPlayerReady] = useState<boolean>(false);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
     SplashScreen.hide();
   });
 
   useEffect(() => {
-    async function run() {
-      const isSetup = await SetupServicePlayer();
-      setIsPlayerReady(isSetup);
+    async function setupPlayer() {
+      await TrackPlayer.setupPlayer();
+      await TrackPlayer.add({
+        // url: 'http://max.miradio.in:8300/stream?type=.mp3',
+        url: 'http://max.miradio.in/proxy/grupobar/stream?type=.mp3',
+        title: 'Potencia Radio',
+        artist: 'Somos Potencia Mundial',
+        artwork: localArtwork,
+      });
 
-      if (isSetup) {
-        TrackPlayer.play();
-      }
-
-      const queue = await TrackPlayer.getQueue();
-      if (isSetup && queue.length <= 0) {
-        await QueueInitalTracksService();
-      }
+      TrackPlayer.updateOptions({
+        android: {
+          appKilledPlaybackBehavior: AppKilledPlaybackBehavior.ContinuePlayback,
+        },
+        capabilities: [
+          Capability.Play,
+          Capability.Pause,
+          Capability.SkipToNext,
+          Capability.SkipToPrevious,
+          Capability.Stop,
+        ],
+        compactCapabilities: [
+          Capability.Play,
+          Capability.Pause,
+          Capability.SkipToNext,
+        ],
+        progressUpdateEventInterval: 2,
+        playIcon: require('../Assets/ic_play.png'),
+        pauseIcon: require('../Assets/ic_pause.png'),
+      });
     }
 
-    run();
+    setupPlayer();
   }, []);
+
+  const togglePlayback = async () => {
+    const state = await TrackPlayer.getState();
+    if (state === State.Playing) {
+      console.log('The player is playing');
+    }
+    if (state === State.Playing) {
+      await TrackPlayer.pause();
+      setIsPlaying(false);
+    } else {
+      await TrackPlayer.play();
+      setIsPlaying(true);
+    }
+  };
+
+  // useEffect(() => {
+  //   async function run() {
+  //     const isSetup = await SetupServicePlayer();
+  //     console.log({isSetup});
+  //     setIsPlayerReady(isSetup);
+
+  //     if (isSetup) {
+  //       TrackPlayer.play();
+  //     }
+
+  //     const queue = await TrackPlayer.getQueue();
+  //     console.log({queue});
+  //     if (isSetup && queue.length <= 0) {
+  //       await QueueInitalTracksService();
+  //     }
+  //   }
+
+  //   run();
+  // }, []);
 
   // Muestra el FCM token en consola.
   useEffect(() => {
     const checkToken = async () => {
       const fcmToken = await messaging().getToken();
       if (fcmToken) {
-        console.log(fcmToken);
+        console.log({fcmToken});
       }
     };
 
@@ -92,14 +153,22 @@ export const HomeScreen = () => {
             justifyContent: 'center',
             alignItems: 'center',
           }}>
-          <PlayPauseButton
+          <View
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>{isPlaying ? 'Reproduciendo' : 'Pausado'}</Text>
+            <Button
+              title={isPlaying ? 'Pausar' : 'Reproducir'}
+              onPress={togglePlayback}
+            />
+          </View>
+          {/* <PlayPauseButton
             OnPressAd={async () => {
               await analytics().logEvent('playRadio', {
                 id: Date.now.toString(),
                 description: 'play/pause Radio',
               });
             }}
-          />
+          /> */}
         </View>
       </LinearGradient>
     </View>
