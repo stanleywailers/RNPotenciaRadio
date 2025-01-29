e5f28232cb710f8baf4f42cdd2ebc65c4d61abdb
douglas <dfjuarezreyes21@gmail.com>
2025-01-29 09:49:39 -0600

    app.ts pretty

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
 import 'react-native-gesture-handler';
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { CustomDrawer } from './src/Navigator/CustomDrawer';
-import { LoopGameScreen } from './src/Screens/LoopGameScreen';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {CustomDrawer} from './src/Navigator/CustomDrawer';
 // import { AdsScreen } from './src/Screens/AdsScreen';
-
-
 
 const App = () => {
   return (
-    <NavigationContainer> 
-    <CustomDrawer/>
+    <NavigationContainer>
+      <CustomDrawer />
     </NavigationContainer>
-  
-  )
-}
-
+  );
+};
 
 export default App;
