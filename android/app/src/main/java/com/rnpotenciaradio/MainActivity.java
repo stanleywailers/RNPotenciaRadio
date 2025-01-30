c186083a100e81c4230dbd030c4304bd3927d82e
douglas <dfjuarezreyes21@gmail.com>
2025-01-30 10:16:45 -0600

    clear api update 34 file

--- android/app/src/main/java/com/rnpotenciaradio/MainActivity.java
+++ android/app/src/main/java/com/rnpotenciaradio/MainActivity.java
@@ -10,9 +10,6 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
-
- 
-
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -33,43 +30,6 @@ public class MainActivity extends ReactActivity {
         this,
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled());
-    // return new DefaultReactActivityDelegate(
-    //     this,
-    //     getMainComponentName(),
-    //     // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-    //     DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
-    //     // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
-    //     DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
-    //     );
-  //   return new MainActivityDelegate(this, getMainComponentName());
-  // }
-
-  // @Override
-  // protected void onCreate(Bundle savedInstanceState) {
-  //     SplashScreen.show(this);  // here
-  //     super.onCreate(savedInstanceState);
-  // }
-
-  // public static class MainActivityDelegate extends ReactActivityDelegate {
-  //   public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
-  //     super(activity, mainComponentName);
-  //   }
-
-  //   @Override
-  //   protected ReactRootView createRootView() {
-  //     ReactRootView reactRootView = new ReactRootView(getContext());
-  //     // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-  //     reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
-  //     return reactRootView;
-  //   }
-
-  //   @Override
-  //   protected boolean isConcurrentRootEnabled() {
-  //     // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
-  //     // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
-  //     return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());    
   }
-
-  
 }
