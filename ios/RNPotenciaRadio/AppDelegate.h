d59e56bf1cf6671bacc93f7af64cc3dfa3021dc6
douglas <dfjuarezreyes21@gmail.com>
2025-01-31 12:50:22 -0600

    ios config build

--- ios/RNPotenciaRadio/AppDelegate.h
+++ ios/RNPotenciaRadio/AppDelegate.h
@@ -1,8 +1,10 @@
-#import <React/RCTBridgeDelegate.h>
+// #import <React/RCTBridgeDelegate.h>
+#import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+// @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
-@property (nonatomic, strong) UIWindow *window;
+// @property (nonatomic, strong) UIWindow *window;
+@interface AppDelegate : RCTAppDelegate
 
 @end
